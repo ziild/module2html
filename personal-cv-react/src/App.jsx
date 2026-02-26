7ca7212a886946367e0e725f2e62f2ae13ac9594
ziild <blazezildjan123@example.com>
2026-02-26 21:17:16 +0800

    WSAT-M5: Migrated CV to ReactJS

--- personal-cv-react/src/App.jsx
+++ personal-cv-react/src/App.jsx
@@ -1,9 +1,9 @@
+import React, { useState } from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Education from "./components/Education";
 import Contact from "./components/Contact";
-import { useState } from "react";
   
 function App() {
   const [darkMode, setDarkMode] = useState(false);
