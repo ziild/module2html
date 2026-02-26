b336099ff91b90cdcf1d7c28bd84693b4bd1eb90
ziild <blazezildjan123@example.com>
2026-02-26 20:53:11 +0800

    WSAT-M5: Migrated CV to ReactJS

--- personal-cv-react/src/components/About.jsx
+++ personal-cv-react/src/components/About.jsx
@@ -1,32 +1,34 @@
+import profilePhoto from './assets/images/profile.jpg'; 
+
 function About() {
   return (
-    <div className = "container">
-    <section className="card">
-      <h2 className="darktext"style={{ textAlign: "center" } }>
-        ABOUT ME
-      </h2>
-      <img src="src/assets/images/profile.jpg" alt="Profile photo"></img>
-      <p>
-        I am a dedicated IT student specializing in Web Systems and
-        Technologies. Passionate about creating efficient and user-friendly
-        web applications, I strive to combine technical expertise with creative
-        problem-solving.
-      </p>
-
-      <p>
-        Email:
-        <a href="mailto:blazezildjan123@gmail.com">
-          blazezildjan123@gmail.com
-        </a>
+    <div className="container">
+      <section className="card">
+        <h2 className="darktext" style={{ textAlign: "center" }}>
+          ABOUT ME
+        </h2>
+        <img src={profilePhoto} alt="Profile photo" />
+        <p>
+          I am a dedicated IT student specializing in Web Systems and
+          Technologies. Passionate about creating efficient and user-friendly
+          web applications, I strive to combine technical expertise with creative
+          problem-solving.
         </p>
 
         <p>
-        GitHub:
-        <a href="https://github.com/ziild" target="_blank" rel="noopener noreferrer">
-          github.com/ziild
-        </a>
-      </p>
-    </section>
+          Email:
+          <a href="mailto:blazezildjan123@gmail.com">
+            blazezildjan123@gmail.com
+          </a>
+        </p>
+
+        <p>
+          GitHub:
+          <a href="https://github.com/ziild" target="_blank" rel="noopener noreferrer">
+            github.com/ziild
+          </a>
+        </p>
+      </section>
     </div>
   );
 }
